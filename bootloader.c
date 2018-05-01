a36f650fecf26d94c73b65e526ef8589a799cc0a
Narek Bojikiain <narekbojikiain@Nareks-MacBook-Air.local>
2018-05-01 21:54:23 +0200

    feature: read pass function implementation

--- bootloader.c
+++ bootloader.c
@@ -6,27 +6,86 @@ asm(".code16gcc\n");
 /* needs to stay the first line */
 asm("jmp $0, $main");
 
-void printhello()
+void print(char str[], int len)
 {
-	char hello[] = "Hello\n\r";
-	long len = sizeof(hello)/sizeof(char);
 	asm("movb $0x0E, %ah;");
-	for(long i = 0; i < len - 1; i++)
+	for(long i = 0; i < len; i++)
 	{
 		asm(
 			"int $0x10;"
 		:
-		:	"a"(hello[i])
+		:	"a"(str[i])
 		);
 	}
+	asm(
+		"mov $10, %al;"
+		"movb $0x0E, %ah;"
+		"int $0x10;"
+		"mov $13, %al;"
+		"movb $0x0E, %ah;"
+		"int $0x10;"
+	);
 }
 
-void readPassowd();
+//
+//int readPassowrd
+//int 0 password read
+//int 1 password empty
+void readPassword()
+{
+//while(1)
+{
+	char in[8];
+	int l;
+	asm(
+		"mov $0, %%si;"
+		"read:"
+		"movb $0x10, %%ah;"
+		"int $0x16;"
+
+		"cmp $13, %%al;"
+		"je end;"
+		"cmp $8, %%si;"
+		"je read;"
+		
+
+		"mov %%ax, (%%bx, %%si);"
+		"inc %%si;"
+		"mov $46, %%al;"
+		"movb $0x0E, %%ah;"
+		"int $0x10;"
+		"jmp read;"
+		"int $0x10;"
+		"jmp read;"
+		"end:"
+		
+		: "=S"(l)
+		: "b"(in)
+	);
+	if(l == 0)
+	{
+		asm("int $0x19;");
+	}
+	else
+	{
+		asm(
+		"mov $10, %al;"
+		"movb $0x0E, %ah;"
+		"int $0x10;"
+		"mov $13, %al;"
+		"movb $0x0E, %ah;"
+		"int $0x10;"
+		);
+	}
+	print(in, l);
+}
+}
 
 /* space for additional code */
 void main(void)
 {
-	printhello();
+	print("Hello", 7);
+	readPassword();
 	asm("jmp .");
 	
 }
