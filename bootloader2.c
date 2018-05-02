ba0dee1ce651871f8c1f3b53ef1d04245e2c275f
pe-wu <piotrekwitkowski1@gmail.com>
2018-05-02 23:27:22 +0200

    Update bootloader2.c

--- bootloader2.c
+++ bootloader2.c
@@ -5,8 +5,7 @@ asm(".code16gcc\n");
 asm("jmp $0, $main");
 
 /////////////MAIN PROGRAM/////////////
-/*
-Ausgabe der Zeichenkette "Hello!"
+/*Ausgabe der Zeichenkette "Hello!"
 Verdeckte Eingabe eines Strings (maximal 8 Zeichen), Ausgabe von Punkten statt Buchstaben
 Ausgabe der verdeckten Eingabe nach Drücken der Enter-Taste
 Neustart des Systems, wenn der Eingabe-String leer ist
@@ -14,30 +13,26 @@ Nach Ein-/Ausgabe einer nicht leeren Zeichenkette soll anschließend die nächst
 */
 
 char HELLO[] = "Hello!";
-char SECRET_CHAR = '*';
+int HELLO_LENGTH = 6;
+char SECRET_CHAR = '.';
+char ENTER = '\r';
+char password[8];
 int MAX_PASSWORD_LENGTH = 8;
-char password[] = "password";
 int passwordLength = 0;
 
 void println(char text[], int maxLength);
 void printLetter(char letter);
-void getPassword();
+int getPassword();
+void saveChar(char letter);
 char readChar();
-int playingWithPasswords();
-void saveLetter(char letter);
 void reboot();
 
 void main() {
-	println(HELLO, 6);
-	while (playingWithPasswords()) {
+	println(HELLO, HELLO_LENGTH);
+	while (getPassword()) {
 		println(password, passwordLength);
 	}
 	reboot();
-}
-
-int playingWithPasswords() {
-	getPassword();
-	return passwordLength;
 }
 
 void println(char text[], int length) {
@@ -48,33 +43,32 @@ void println(char text[], int length) {
 	printLetter('\r');
 }
 
-void getPassword() {
+int getPassword() {
 	passwordLength = 0;
-	
 	while (1) {
 		char lastChar = readChar();
-		if (lastChar != '\r') {
-			saveLetter(lastChar);
-		} else {
-			break;
-		}
+		if (lastChar == ENTER) break;
+		saveChar(lastChar);
 	}
-	printLetter('\n');
-	printLetter('\r');
+	println("", 0);
+	return passwordLength;
 }
 
-void saveLetter(char letter) {
+void saveChar(char letter) {
 	if (passwordLength < MAX_PASSWORD_LENGTH) {
 		password[passwordLength] = letter;
-		printLetter(SECRET_CHAR);
 		passwordLength++;
+		printLetter(SECRET_CHAR);
 	}
 }
 
 void printLetter(char letter) {
 	asm(
-		"movb $0x0e, %%ah;" //INTERRUPT CODE FOR OUTPUT
-		"int  $0x10"::"a"(letter));
+		"movb $0x0e, %%ah;" 	//INT FCODE FOR VIDEO - TELETYPE OUTPUT
+		"int  $0x10;"
+		:
+		:"al"(letter)
+	);
 }
 
 char readChar() {
@@ -82,15 +76,14 @@ char readChar() {
 	printLetter((char)0x08); //BACKSPACE
 	char lastChar;
 	asm(
-		"movb $0x0, %%ah;" //INTERRUPT CODE FOR READ
+		"movb $0x0, %%ah;"		//INT FCODE FOR KEYBOARD - GET KEYSTROKE
         "int $0x16"
 		:"=al"(lastChar)
 		:
 	);
-	
 	return lastChar;
 }
 
 void reboot() {
-	asm("int $0x19;");
+	asm("int $0x19;");			//INT FCODE FOR SYSTEM - BOOTSTRAP LOADER
 }
